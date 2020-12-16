72fd71b52569c9986288662627c95d8d8e5f34c4
pbrzezinski <contact.brzezinski@gmail.com>
2020-12-16 17:57:06 +0100

    SiteMap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,6 @@
 module.exports = {
   siteMetadata: {
+    siteUrl: 'https://twojeznamiona.pl',
     title: 'Gatsby + Netlify CMS Starter',
     description:
       'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
@@ -82,6 +83,37 @@ module.exports = {
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        serialize: ({ site, allSitePage }) => {
+          const { allPages } = getNodes(allSitePage)
+          return allPages?.map(page => {
+            return {
+              url: `${site.siteMetadata?.siteUrl ?? ``}${page.path}`,
+              lastmodISO: new Date().toISOString(),
+              priority: 0.9,
+            }
+          })
+        },
+      },
+    },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
+}
+
+function getNodes(results) {
+  if (`nodes` in results) {
+    return { allPages: results.nodes, originalType: `nodes` }
+  }
+
+  if (`edges` in results) {
+    return {
+      allPages: results?.edges?.map(edge => edge.node),
+      originalType: `edges`,
+    }
+  }
+  throw new Error(
+    `[gatsby-plugin-sitemap]: Plugin is unsure how to handle the results of your query, you'll need to write custom page filter and serializer in your gatsby config`
+  )
 }
