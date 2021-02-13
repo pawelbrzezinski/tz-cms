adca293553eefba14e712fccfd4b7f44c3ea8221
pbrzezinski <contact.brzezinski@gmail.com>
2021-02-13 18:49:14 +0100

    o nas lekarze

--- src/pages/o-nas/Doctor.tsx
+++ src/pages/o-nas/Doctor.tsx
@@ -4,9 +4,20 @@ import Button from "../../components/Button";
 import "../../styles/doctor.scss";
 import Modal from "../../components/Modal";
 
+const SplitedDescription = ({text = ""}) => {
+  const sentences = text.split(".");
+
+  return sentences.length > 5 ? (
+    <>
+      <p>{sentences.slice(0, sentences.length / 2).join(" ")}</p>
+      <p>{sentences.slice(sentences.length / 2).join(" ")}</p>
+    </>
+  ) : <p>{text}</p>;
+};
+
 const Doctor = ({
   className = "",
-  data = { name: "", title: "", img : null, city: "", description: "" },
+  data = { name: "", title: "", img: null, city: "", description: "" },
 }) => {
   const [isModalOpened, setModalOpened] = useState(false);
 
@@ -33,8 +44,11 @@ const Doctor = ({
                 <div className="doctor_modal_body_icon_wrapper">
                   <img src={data.img} alt={data.name} />
                 </div>
-                <div>
-                  <h5>{data.description}</h5>
+                <div className="doctor_modal_body_name">{data.name}</div>
+                <div className="doctor_modal_body_title">{data.title}</div>
+                {data.city && <div className="doctor_modal_body_city">{data.city}</div>}
+                <div className="doctor_modal_body_description">
+                  <SplitedDescription text={data.description} />
                 </div>
               </div>
             </Modal>
