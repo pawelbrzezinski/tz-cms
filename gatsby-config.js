206546c10ee6808d7ba0f4105263577a690a70cb
pbrzezinski <contact.brzezinski@gmail.com>
2021-02-23 23:41:01 +0100

    solving the build issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,12 +97,6 @@ module.exports = {
         ],
       },
     },
-    // {
-    //   resolve: 'gatsby-plugin-netlify-cms',
-    //   options: {
-    //     modulePath: `${__dirname}/src/cms/cms.js`,
-    //   },
-    // },
     {
       resolve: `gatsby-plugin-material-ui`,
       // options: {
@@ -141,6 +135,12 @@ module.exports = {
             }
           })
         },
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
     'gatsby-plugin-client-side-redirect', // keep it in last in list
