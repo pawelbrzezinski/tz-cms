de997032b5c9faf56c1962609017ab3c5a9e5a11
pbrzezinski <contact.brzezinski@gmail.com>
2021-03-09 15:36:18 +0100

    empty sources and tags rendeeriing

--- src/components/cms/Sources.tsx
+++ src/components/cms/Sources.tsx
@@ -3,26 +3,23 @@ import { Link } from "gatsby";
 import "../../styles/cms.scss";
 
 const Sources = ({ data = [] }) => {
-  return (
-    data &&
-    data.length && (
-      <div className="article-sources">
-        <h4>Źródła:</h4>
-        <ol>
-          {data.map((item) => (
-            <li key={`${item.label}_${item.link}`}>
-              {item.link ? (
-                <a href={`${item.link}`} target="_blank" title={item.label}>
-                  {item.label || item.link}
-                </a>
-              ) : (
-                item.label
-              )}
-            </li>
-          ))}
-        </ol>
-      </div>
-    )
-  );
+  return data && data.length ? (
+    <div className="article-sources">
+      <h4>Źródła:</h4>
+      <ol>
+        {data.map((item) => (
+          <li key={`${item.label}_${item.link}`}>
+            {item.link ? (
+              <a href={`${item.link}`} target="_blank" title={item.label}>
+                {item.label || item.link}
+              </a>
+            ) : (
+              item.label
+            )}
+          </li>
+        ))}
+      </ol>
+    </div>
+  ) : null;
 };
 export default Sources;
--- src/components/cms/Tags.tsx
+++ src/components/cms/Tags.tsx
@@ -5,23 +5,19 @@ import { kebabCase } from "lodash";
 import "../../styles/cms.scss";
 
 const Tags = ({ data = [] }) => {
-  return (
-    data &&
-    data &&
-    data.length && (
-      <div className="article-tags">
-        {data.map((tag) => (
-          <Link
-            to={`/tags/${kebabCase(tag)}/`}
-            key={tag + `tag`}
-            className="tag_item"
-          >
-            {tag}
-          </Link>
-        ))}
-      </div>
-    )
-  );
+  return data && data.length ? (
+    <div className="article-tags">
+      {data.map((tag) => (
+        <Link
+          to={`/tags/${kebabCase(tag)}/`}
+          key={tag + `tag`}
+          className="tag_item"
+        >
+          {tag}
+        </Link>
+      ))}
+    </div>
+  ) : null;
 };
 
 export default Tags;
