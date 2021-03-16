ff032a2e94134208ac8e2e2e99d6c7f1a1d0f9b1
pbrzezinski <contact.brzezinski@gmail.com>
2021-03-16 16:28:29 +0100

    for public forlder with htacces and sendmail.php

---
+++ forPublic/sendmail.php
@@ -0,0 +1,26 @@
+﻿<?php
+if(isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['mail']) && isset($_POST['msg']) && isset($_POST['phone'])){
+  $name = addslashes(str_replace('>','&gt;',str_replace('<','&lt;',$_POST['name'])));
+  $surname = addslashes(str_replace('>','&gt;',str_replace('<','&lt;',$_POST['surname'])));
+  $mail = addslashes(str_replace('>','&gt;',str_replace('<','&lt;',$_POST['mail'])));
+  $phone = addslashes(str_replace('>','&gt;',str_replace('<','&lt;',$_POST['phone'])));
+  $miasto = addslashes(str_replace('>','&gt;',str_replace('<','&lt;',$_POST['miasto'])));
+  $text = addslashes(str_replace('>','&gt;',str_replace('<','&lt;',$_POST['msg'])));
+  $data = date('d-m-Y');
+  $czas = date('G:i');
+  $data = $data.' ('.$czas.')';
+  $ip = $_SERVER['REMOTE_ADDR'];
+
+  //spr. poprawnosci podanego maila
+    //jak bedziemy na serwerze to odkomentowac to zeby wysylalo maila
+    mail("zbadamy@twojeznamiona.pl",
+        'Wiadomosc od pacjenta',
+        'Od: '.$name.' '.$surname.'<br />email: '.$mail."<br />tel: ".$phone."<br />data: ".$data."<br />miasto: ".$miasto."<br /><br />".$text,
+        "From: zbadamy@twojeznamiona.pl \r\nReply-To: ".$mail."\r\nContent-type: text/html; charset=utf-8",
+        '-fzbadamy@twojeznamiona.pl'
+        );
+    //przekazanie informacji o powodzeniu uperacji
+    echo '1';
+}
+
+?>
