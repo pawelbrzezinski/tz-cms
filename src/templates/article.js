b7f04add11e808d9a719f030163e0b48aee3b35d
pbrzezinski <contact.brzezinski@gmail.com>
2021-04-30 20:54:24 +0200

    spis tresci na strone blogowa

--- src/templates/article.js
+++ src/templates/article.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { Helmet } from 'react-helmet'
 import Layout from '../components/Layout'
 import { graphql } from 'gatsby'
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import { MDXProvider } from "@mdx-js/react"
 import Content from '../components/Content'
+import CloseIcon from "../img/close.svg";
 
 import MyArticleTable from '../components/cms/MyArticleTable'
 import MyArticleImg from '../components/cms/MyArticleImg'
 import MyArticleMore from '../components/cms/MyArticleMore'
 import YoutubePlayer from "../components/YoutubePlayer";
 import YoutubeGif from "../components/YoutubeGif";
-
+import TocIcon from "../img/toc_icon.svg";
 import Sources from '../components/cms/Sources'
 import Tags from '../components/cms/Tags'
 import AuthorHeader from '../components/cms/AuthorHeader'
@@ -37,7 +38,7 @@ const components = {
     )
   },
   h3: (props) => {
-   const className = hasWidederm(props.children) ? "wideoderm" : "";
+    const className = hasWidederm(props.children) ? "wideoderm" : "";
 
     return (
       <h3 className={className}>
@@ -48,6 +49,49 @@ const components = {
   YoutubePlayer,
   YoutubeGif
 };
+
+const Toc = ({ toc }) => {
+
+  const [isMobileToc, setIsMobileToc] = useState(false);
+  const openMobileToc = useCallback(() => {
+    setIsMobileToc(true);
+  }, [setIsMobileToc]);
+
+  const closeMobileToc = useCallback(() => {
+    setIsMobileToc(false);
+  }, [setIsMobileToc]);
+
+
+  return (
+    <>
+      <div className="mobile_toc_button" onClick={openMobileToc}>
+        <img src={TocIcon} />
+        <span>
+          Spis treści
+        </span>
+      </div>
+      <div className={"side_toc" + (isMobileToc ? " side_toc--mobile-opened" : "")}>
+        <div className="side_toc_label_wrapper">
+          <h4>Spis treści:</h4>
+          <img className="closeIcon" src={CloseIcon} onClick={closeMobileToc} alt="Zamknij Spis trści" />
+        </div>
+        <div className="sticky">
+
+          {toc && toc.length && (<ol>
+            <li onClick={closeMobileToc}>
+              <AnchorLink offset='100' href={`#intro`} title="Wstęp">Wstęp</AnchorLink>
+            </li>
+            {toc.map(item => (
+              <li key={item.label} onClick={closeMobileToc}>
+                <AnchorLink offset='60' href={`#${item.link}`} title={item.label}>{item.label}</AnchorLink>
+              </li>
+            ))}
+          </ol>)}
+        </div>
+      </div>
+    </>
+  )
+}
 
 const ArticlePage = (props) => {
   const { title, description, keywords, rawDate, toc = [], sources = [], date, author, authorsTitle, readingTime, tags } = props.data.mdx.frontmatter
@@ -68,21 +112,7 @@ const ArticlePage = (props) => {
         />
 
         <div className="article_container">
-          <div className="side_toc">
-            <div className="sticky">
-              <h4>Spis treści:</h4>
-              {toc && toc.length && (<ol>
-                <li>
-                  <AnchorLink offset='100' href={`#intro`} title="Wstęp">Wstęp</AnchorLink>
-                </li>
-                {toc.map(item => (
-                  <li key={item.label}>
-                    <AnchorLink offset='60' href={`#${item.link}`} title={item.label}>{item.label}</AnchorLink>
-                  </li>
-                ))}
-              </ol>)}
-            </div>
-          </div>
+          <Toc toc={toc} />
           <div className="article_content_wrapper">
             <span id="intro" />
             <h1>
